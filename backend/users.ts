0b43f81faff085b8ef510b82fc339a43cda2ad84
unknown <jose.wollinger@gmail.com>
2018-10-13 20:41:54 -0300

    initial commit

---
+++ backend/users.ts
@@ -0,0 +1,16 @@
+export class User {
+    constructor(public email: string,
+        public name: string,
+        private password: string) { }
+
+    macthes(another: User): boolean {
+        return another !== undefined && 
+            another.email === this.email && 
+            another.password === this.password
+    }
+}
+
+export const users:{[key:string]: User} = {
+    "luizbsilva@gmail.com": new User('luizbsilva@gmail.com', 'Luiz B Silva','luiz23'),
+    "luizsilva@gmail.com": new User('luizsilva@gmail.com', 'Luiz Silva', 'silva23')
+}
